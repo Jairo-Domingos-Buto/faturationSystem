9cca0afa91a8e964582dd46e5a06cb965db1aa8e
Jairo Domingos Buto <jairobuto03@gmail.com>
2025-10-12 01:39:23 +0100

    [feat]:Autenticacao inplementada sem back end

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login',function(){
+    return view('auth.login');
+});
+Route::get('/register', function () {
+  return view('auth.register');
+});
+Route::get('/forgetPassword', function () {
+  return view('auth.forgetPassword');
+});
+
+
 Route::get('/', function () {
-    return redirect('/dashboard');
+    return redirect('/login');
 });
 Route::get('/dashboard', function () {
     return view('Admin/dashboard');
